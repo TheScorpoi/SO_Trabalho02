a57e14691ecbe6eda5a4b739ff595036ee5a2f74
TheScorpoi <sobral@ua.pt>
2021-01-18 14:20:44 +0000

    alterations on player

--- semaphore_soccergame/src/semSharedMemPlayer.c
+++ semaphore_soccergame/src/semSharedMemPlayer.c
@@ -15,20 +15,20 @@
  *  \author Nuno Lau - January 2021
  */
 
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <string.h>
-#include <math.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#include "logging.h"
 #include "probConst.h"
 #include "probDataStruct.h"
-#include "logging.h"
-#include "sharedDataSync.h"
 #include "semaphore.h"
+#include "sharedDataSync.h"
 #include "sharedMemory.h"
 
 /** \brief logging file name */
@@ -44,10 +44,10 @@ static int semgid;
 static SHARED_DATA *sh;
 
 /** \brief player takes some time to arrive */
-static void arrive (int id);
+static void arrive(int id);
 
 /** \brief player constitutes team */
-static int playerConstituteTeam (int id);
+static int playerConstituteTeam(int id);
 
 /** \brief player waits for referee to start match */
 static void waitReferee(int id, int team);
@@ -60,71 +60,68 @@ static void playUntilEnd(int id, int team);
  *
  *  Its role is to generate the life cycle of one of intervening entities in the problem: the player.
  */
-int main (int argc, char *argv[])
-{
-    int key;                                            /*access key to shared memory and semaphore set */
-    char *tinp;                                                       /* numerical parameters test flag */
+int main(int argc, char *argv[]) {
+    int key;    /*access key to shared memory and semaphore set */
+    char *tinp; /* numerical parameters test flag */
     int n, team;
 
     /* validation of command line parameters */
-    if (argc != 4) { 
-        freopen ("error_PL", "a", stderr);
-        fprintf (stderr, "Number of parameters is incorrect!\n");
+    if (argc != 4) {
+        freopen("error_PL", "a", stderr);
+        fprintf(stderr, "Number of parameters is incorrect!\n");
         return EXIT_FAILURE;
     }
-    
 
     /* get goalie id - argv[1]*/
-    n = (unsigned int) strtol (argv[1], &tinp, 0);
-    if ((*tinp != '\0') || (n >= NUMPLAYERS )) { 
-        fprintf (stderr, "Player process identification is wrong!\n");
+    n = (unsigned int)strtol(argv[1], &tinp, 0);
+    if ((*tinp != '\0') || (n >= NUMPLAYERS)) {
+        fprintf(stderr, "Player process identification is wrong!\n");
         return EXIT_FAILURE;
     }
 
     /* get logfile name - argv[2]*/
-    strcpy (nFic, argv[2]);
+    strcpy(nFic, argv[2]);
 
     /* redirect stderr to error file  - argv[3]*/
-    freopen (argv[3], "w", stderr);
-    setbuf(stderr,NULL);
-
+    freopen(argv[3], "w", stderr);
+    setbuf(stderr, NULL);
 
     /* getting key value */
-    if ((key = ftok (".", 'a')) == -1) {
-        perror ("error on generating the key");
-        exit (EXIT_FAILURE);
+    if ((key = ftok(".", 'a')) == -1) {
+        perror("error on generating the key");
+        exit(EXIT_FAILURE);
     }
 
     /* connection to the semaphore set and the shared memory region and mapping the shared region onto the
        process address space */
-    if ((semgid = semConnect (key)) == -1) { 
-        perror ("error on connecting to the semaphore set");
+    if ((semgid = semConnect(key)) == -1) {
+        perror("error on connecting to the semaphore set");
         return EXIT_FAILURE;
     }
-    if ((shmid = shmemConnect (key)) == -1) { 
-        perror ("error on connecting to the shared memory region");
+    if ((shmid = shmemConnect(key)) == -1) {
+        perror("error on connecting to the shared memory region");
         return EXIT_FAILURE;
     }
-    if (shmemAttach (shmid, (void **) &sh) == -1) { 
-        perror ("error on mapping the shared region on the process address space");
+    if (shmemAttach(shmid, (void **)&sh) == -1) {
+        perror("error on mapping the shared region on the process address space");
         return EXIT_FAILURE;
     }
 
     /* initialize random generator */
-    srandom ((unsigned int) getpid ());                                                 
-
+    srandom((unsigned int)getpid());
 
     /* simulation of the life cycle of the player */
     arrive(n);
-    if((team = playerConstituteTeam(n))!=0) {
+    if ((team = playerConstituteTeam(n)) != 0) {
         waitReferee(n, team);
         playUntilEnd(n, team);
     }
 
     /* unmapping the shared region off the process address space */
-    if (shmemDettach (sh) == -1) {
-        perror ("error on unmapping the shared region off the process address space");
-        return EXIT_FAILURE;;
+    if (shmemDettach(sh) == -1) {
+        perror("error on unmapping the shared region off the process address space");
+        return EXIT_FAILURE;
+        ;
     }
 
     return EXIT_SUCCESS;
@@ -137,23 +134,22 @@ int main (int argc, char *argv[])
  *  The internal state should be saved.
  *
  */
-static void arrive(int id)
-{    
-    if (semDown (semgid, sh->mutex) == -1)  {                                                     /* enter critical region */
-        perror ("error on the up operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+static void arrive(int id) {
+    if (semDown(semgid, sh->mutex) == -1) { /* enter critical region */
+        perror("error on the up operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
     /* TODO: insert your code here */
     sh->fSt.st.playerStat[id] = ARRIVING;
     saveState(nFic, &sh->fSt);
 
-    if (semUp (semgid, sh->mutex) == -1) {                                                         /* exit critical region */
-        perror ("error on the down operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+    if (semUp(semgid, sh->mutex) == -1) { /* exit critical region */
+        perror("error on the down operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
-    usleep((200.0*random())/(RAND_MAX+1.0)+50.0);
+    usleep((200.0 * random()) / (RAND_MAX + 1.0) + 50.0);
 }
 
 /**
@@ -171,60 +167,47 @@ static void arrive(int id)
  *  \return id of player team (0 for late goalies; 1 for team 1; 2 for team 2)
  *
  */
-static int playerConstituteTeam (int id)
-{
+static int playerConstituteTeam(int id) {
     int ret = 0;
 
-    if (semDown (semgid, sh->mutex) == -1)  {                                                     /* enter critical region */
-        perror ("error on the up operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+    if (semDown(semgid, sh->mutex) == -1) { /* enter critical region */
+        perror("error on the up operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
-
 
     /* TODO: insert your code here */
     sh->fSt.playersFree++;
     sh->fSt.playersArrived++;
 
     /* TODO: insert your code here */
-    if (sh->fSt.playersArrived <= 8)
-    {
-        if (sh->fSt.playersFree >= 4 && sh->fSt.goaliesFree >= 1)
-        {
-
+    if (sh->fSt.playersArrived <= 8) {
+        if (sh->fSt.playersFree >= 4 && sh->fSt.goaliesFree >= 1) {
             sh->fSt.st.playerStat[id] = FORMING_TEAM;
-            sh->fSt.playersFree = sh->fSt.playersFree - 4; 
+            sh->fSt.playersFree = sh->fSt.playersFree - 4;
             sh->fSt.goaliesFree--;
             saveState(nFic, &sh->fSt);
-        }
-        else
-        {
+        } else {
             sh->fSt.st.playerStat[id] = WAITING_TEAM;
             saveState(nFic, &sh->fSt);
         }
-        
-    }
-    else
-    {
+
+    } else {
         sh->fSt.st.goalieStat[id] = LATE;
         saveState(nFic, &sh->fSt);
     }
 
-
-    if (semUp (semgid, sh->mutex) == -1) {                                                         /* exit critical region */
-        perror ("error on the down operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+    if (semUp(semgid, sh->mutex) == -1) { /* exit critical region */
+        perror("error on the down operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
     /* TODO: insert your code here */
-    for (int i = 0; i < NUMPLAYERS; i++)
-    {
-        if (semUp(semgid, sh->playersWaitTeam) == -1)
-        {
+    for (int i = 0; i < NUMPLAYERS; i++) {
+        if (semUp(semgid, sh->playersWaitTeam) == -1) {
             perror("error on the down operation for semaphore access (GL)");
             exit(EXIT_FAILURE);
         }
     }
-
 
     return ret;
 }
@@ -238,22 +221,30 @@ static int playerConstituteTeam (int id)
  *  \param id   player id
  *  \param team player team
  */
-static void waitReferee (int id, int team)
-{
-    if (semDown (semgid, sh->mutex) == -1)  {                                                     /* enter critical region */
-        perror ("error on the up operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+static void waitReferee(int id, int team) {
+    if (semDown(semgid, sh->mutex) == -1) { /* enter critical region */
+        perror("error on the up operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
-    /* TODO: insert your code here */
+    // TODO: insert your code here
+    if (team == 1) {
+        sh->fSt.st.playerStat[id] = WAITING_START_1;
+    } else if (team == 2) {
+        sh->fSt.st.playerStat[id] = WAITING_START_2;
+    }
+    saveState(nFic, &sh->fSt);
 
-    if (semUp (semgid, sh->mutex) == -1) {                                                         /* exit critical region */
-        perror ("error on the down operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+    if (semUp(semgid, sh->mutex) == -1) { /* exit critical region */
+        perror("error on the down operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
-    /* TODO: insert your code here */
-
+    // TODO: insert your code here
+    if (semDown(semgid, sh->playersWaitReferee) == -1) {
+        perror("error on the up operation for semaphore access(PL)");
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
@@ -265,23 +256,28 @@ static void waitReferee (int id, int team)
  *  \param id   player id
  *  \param team player team
  */
-static void playUntilEnd (int id, int team)
-{
-    if (semDown (semgid, sh->mutex) == -1)  {                                                     /* enter critical region */
-        perror ("error on the up operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+static void playUntilEnd(int id, int team) {
+    if (semDown(semgid, sh->mutex) == -1) { /* enter critical region */
+        perror("error on the up operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
-    /* TODO: insert your code here */
+    // TODO: insert your code here
+    if (team == 1) {
+        sh->fSt.st.playerStat[id] = PLAYING_1;
+    } else if (team == 2) {
+        sh->fSt.st.playerStat[id] = PLAYING_2;
+    }
+    saveState(nFic, &sh->fSt);
 
-    if (semUp (semgid, sh->mutex) == -1) {                                                         /* exit critical region */
-        perror ("error on the down operation for semaphore access (PL)");
-        exit (EXIT_FAILURE);
+    if (semUp(semgid, sh->mutex) == -1) { /* exit critical region */
+        perror("error on the down operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
     }
 
-    /* TODO: insert your code here */
-
+    // TODO: insert your code here
+    if (semDown(semgid, sh->playersWaitEnd) == -1) {
+        perror("error on the up operation for semaphore access (PL)");
+        exit(EXIT_FAILURE);
+    }
 }
-
-
-
